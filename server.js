b43b7e671f5664fb96973c2af9fdd8d2628d05e9
jacob sierra <jacobisaac22@yahoo.com>
2021-09-02 18:05:28 -0500

    created route to view all workouts

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const db = require("./models")
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -10,10 +10,10 @@ app.use (express.json());
 
 app.use(express.static('public'));
 
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessTracker", {
-//   useNewUrlParser: true,
-//   useFindAndModify: false
-// });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessTracker", {
+  useNewUrlParser: true,
+  // useFindAndModify: false
+});
 
 app.use(require("./routes/api.js"));
 
