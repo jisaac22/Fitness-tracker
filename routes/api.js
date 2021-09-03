b236b4258fbbe90c3326fca3bf382cd8e3daeec9
jacob sierra <jacobisaac22@yahoo.com>
2021-09-02 22:07:21 -0500

    updated routes to create a new workout

--- routes/api.js
+++ routes/api.js
@@ -3,8 +3,18 @@ const express = require('express');
 const router = express.Router();
 const Workout = require('../models/Workout.js');
 
-router.get("/api/all", (req, res) => {
+router.get("/api/workouts", (req, res) => {
   Workout.find({})
+  .then(dbWorkout => {
+    res.json(dbWorkout)
+  })
+  .catch(err => {
+    res.status(400).json(err)
+  });
+});
+
+router.post("/api/workouts", ({ body }, res) => {
+  Workout.create(body)
   .then(dbWorkout => {
     res.json(dbWorkout)
   })
